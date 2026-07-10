a66624330d7cc02f859e1f4948fa21d1749f1788
khushidesai23 <kmdesai23@gmail.com>
2026-07-10 10:51:52 +0530

    Refactor configuration management with structured types and enhanced loading logic

--- config/config.go
+++ config/config.go
@@ -1,46 +1,33 @@
 package config
 
-import (
-	"log"
-
-	"github.com/spf13/viper"
-)
-
 type Config struct {
-	AppName string
-	AppEnv  string
-	AppPort string
-
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
-	DBSSLMode  string
-
-	LogLevel string
+	App AppConfig
+	DB  DatabaseConfig
+	Log LogConfig
 }
 
-var AppConfig Config
+type AppConfig struct {
+	Name string
+	Env  string
+	Port string
+}
 
-func LoadConfig() {
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	SSLMode  string
+}
 
-	viper.SetConfigFile(".env")
+type LogConfig struct {
+	Level string
+}
 
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatal(err)
-	}
+var cfg *Config
 
-	AppConfig = Config{
-		AppName:    viper.GetString("APP_NAME"),
-		AppEnv:     viper.GetString("APP_ENV"),
-		AppPort:    viper.GetString("APP_PORT"),
-		DBHost:     viper.GetString("DB_HOST"),
-		DBPort:     viper.GetString("DB_PORT"),
-		DBUser:     viper.GetString("DB_USER"),
-		DBPassword: viper.GetString("DB_PASSWORD"),
-		DBName:     viper.GetString("DB_NAME"),
-		DBSSLMode:  viper.GetString("DB_SSLMODE"),
-		LogLevel:   viper.GetString("LOG_LEVEL"),
-	}
+// Get returns the loaded application configuration.
+func Get() *Config {
+	return cfg
 }
