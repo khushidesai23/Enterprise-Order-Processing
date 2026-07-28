3737ac22e8a6c1caa830de56834dfba58eee876a
khushidesai23 <kmdesai23@gmail.com>
2026-07-28 11:40:02 +0530

    Add Swagger documentation and integrate Swagger UI for API

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -26,7 +26,14 @@ import (
 	"github.com/khushidesai23/Enterprise-Order-Processing/internal/modules/user"
 	"github.com/khushidesai23/Enterprise-Order-Processing/internal/repository"
 	"github.com/khushidesai23/Enterprise-Order-Processing/pkg/logger"
+	_ "github.com/khushidesai23/Enterprise-Order-Processing/docs"
 )
+
+// @title Enterprise Order Processing API
+// @version 1.0
+// @description Enterprise Order Processing & Payment Platform
+// @host localhost:8080
+// @BasePath /api/v1
 
 func main() {
 
